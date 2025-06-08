9b36d54a84a7ba0a4b8f34a63af31e25cb7c4ede
Denis Subasic <denissubasic1221@gmail.com>
2025-06-08 22:01:30 +0200

    Uklonjen db.js iz .gitignore i dodan u repozitorij

---
+++ src/api/db.js
@@ -0,0 +1,18 @@
+import mysql from 'mysql2/promise'
+import dotenv from 'dotenv'
+import process from 'process'
+
+dotenv.config() // Učitaj varijable iz .env
+
+const config = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: 3306,
+  database: process.env.DB_NAME,
+}
+
+export const getConnection = async () => {
+  const connection = await mysql.createConnection(config)
+  return connection
+}
