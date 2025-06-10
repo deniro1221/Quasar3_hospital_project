da7ef76ac4952ca7267a95e30a680f6e0621ad02
Denis Subasic <denissubasic1221@gmail.com>
2025-06-10 12:08:53 +0200

    McLaren

--- src/api/db.js
+++ src/api/db.js
@@ -2,13 +2,19 @@ import mysql from 'mysql2/promise'
 import dotenv from 'dotenv'
 import process from 'process'
 
-dotenv.config()
+dotenv.config({ path: 'C:\\thalasso_projekt\\quasar-project\\.env' })
 
+// Provjera učitanih varijabli
 console.log('DB_HOST:', process.env.DB_HOST)
+console.log('DB_PORT:', process.env.DB_PORT)
+console.log('DB_USER:', process.env.DB_USER)
+console.log('DB_PASS:', process.env.DB_PASS)
+console.log('DB_NAME:', process.env.DB_NAME)
 
+// Konfiguracija
 const config = {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT, 10), // osigurajte da je broj
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
@@ -17,6 +23,12 @@ const config = {
 console.log('Kreirana konfiguracija:', config)
 
 export const getConnection = async () => {
-  const connection = await mysql.createConnection(config)
-  return connection
+  try {
+    const connection = await mysql.createConnection(config)
+    console.log('Uspješno povezan na bazu')
+    return connection
+  } catch (error) {
+    console.error('Greška pri povezivanju na bazu:', error)
+    throw error
+  }
 }
