9328e7d60ea383fe559a2c57cb42389bacbf2c45
deniro1221 <subasicdenis771@gmail.com>
2025-06-28 11:09:07 +0200

    fix tempalte

--- src/pages/historyMenuPage.vue
+++ src/pages/historyMenuPage.vue
@@ -1,4 +1,3 @@
-Copy
 <template>
   <q-page class="q-pa-md">
     <!-- Unos menija -->
@@ -8,6 +7,7 @@ Copy
       </q-card-section>
       <q-card-section>
         <q-form @submit="addMenu" ref="menuForm">
+          <!-- Dodan ref na formu -->
           <q-input
             v-model="form.date"
             type="date"
@@ -40,27 +40,31 @@ Copy
           <template v-slot:body-cell="props">
             <q-td
               :props="props"
-              @dblclick="onCellDblClick(props.row, props.col)"
+              @dblclick="enableEditing(props.row, props.col.name)"
               style="cursor: pointer"
             >
               <!-- Dodan style za bolji UX -->
               <div
                 v-if="
-                  editingCell.rowId === props.row.Datum_marende &&
-                  editingCell.col === props.col.name
+                  !props.row.editing ||
+                  props.col.name === 'Datum_marende' ||
+                  props.col.name === 'username' ||
+                  props.col.name === 'ID_kuhara'
                 "
               >
-                <q-input
-                  v-model="props.row[props.col.field]"
-                  @input="onCellInput(props.row, props.col, $event)"
-                  @blur="cancelEdit()"
-                  dense
-                  autofocus
-                />
-              </div>
-              <div v-else>
                 {{ props.value }}
               </div>
+              <q-input
+                v-else
+                v-model="props.row[props.col.field]"
+                @blur="
+                  () => {
+                    updateMenu(props.row, props.col.field)
+                  }
+                "
+                dense
+                autofocus
+              />
             </q-td>
           </template>
 
@@ -72,12 +76,10 @@ Copy
           </template>
         </q-table>
       </q-card-section>
-      <q-card-section>
-        <q-btn color="primary" @click="confirmUpdate">Ažuriraj meni</q-btn>
-      </q-card-section>
     </q-card>
   </q-page>
 </template>
+
 <script>
 import { ref, onMounted } from 'vue'
 
