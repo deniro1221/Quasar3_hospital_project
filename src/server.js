39526a8b7670d9ff5daf2ab8b35e080acbb65680
deniro1221 <subasicdenis771@gmail.com>
2025-07-04 13:02:56 +0200

    good bye production

--- src/server.js
+++ src/server.js
@@ -14,18 +14,9 @@ import inputPatientRouter from './api/inputPatient.js'
 
 const app = express()
 
-const port = process.env.PORT || 10000
+const port = process.env.PORT || 3000
 
-const corsOptions = {
-  origin: 'https://thalassockmenu.netlify.app', // Točna domena tvog frontenda
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Ako koristiš kolačiće ili Authorization headere
-  optionsSuccessStatus: 204, // Neki stariji browseri zahtijevaju 204 umjesto 200
-}
-
-app.use(cors(corsOptions)) // Koristi corsOptions
-// Eksplicitno dozvoli OPTIONS preflight zahtjeve za sve rute
-app.options('*', cors(corsOptions))
+app.use(cors())
 
 app.use(bodyParser.json())
 
@@ -50,8 +41,8 @@ app.use('/', meniRouter)
 // Input pacijent
 app.use('/', inputPatientRouter)
 
-app.listen(port, () => {
-  console.log(`Server je pokrenut na http://localhost:${port}`)
+app.listen(port, '192.168.1.10', () => {
+  console.log(`Server je pokrenut na http://192.168.1.10:${port}`)
 })
 
 //frontned app you can start on: https://thalassockmenu.netlify.app/
