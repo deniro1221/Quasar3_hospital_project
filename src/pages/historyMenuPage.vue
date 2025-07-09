a95126f29d60c154a6733e35751c76598eefb8bd
deniro1221 <subasicdenis771@gmail.com>
2025-07-09 19:44:38 +0200

    chage date format

--- src/pages/historyMenuPage.vue
+++ src/pages/historyMenuPage.vue
@@ -302,9 +302,9 @@ const fetchMenus = async () => {
         acc[date].username = menu.username || ''
         acc[date].ID_kuhara = menu.ID_kuhara || ''
       } else if (menu.marendaa === 'Marenda2') {
-        acc[date].Juha_m2 = menu.Juha_m2 || ''
-        acc[date].Glavno_jelo_m2 = menu.Glavno_jelo_m2 || ''
-        acc[date].Salata_m2 = menu.Salata_m2 || ''
+        acc[date].Juha_m2 = menu.Juha_m1 || ''
+        acc[date].Glavno_jelo_m2 = menu.Glavno_jelo_m1 || ''
+        acc[date].Salata_m2 = menu.Salata_m1 || ''
         acc[date].username = menu.username || ''
         acc[date].ID_kuhara = menu.ID_kuhara || ''
       }
